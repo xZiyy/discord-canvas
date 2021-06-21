52feccdae67021ee4f271cadca0b6fc696b34249
Hardianto~ <71861395+hardiantojek93@users.noreply.github.com>
2021-06-21 21:52:27 +0800

    Update Goodbye.js

--- src/greetings/Goodbye.js
+++ src/greetings/Goodbye.js
@@ -1,8 +1,8 @@
 const Greeting = require("./Base");
 const anu = ["GOODBYE", "INNALILAHI", "ALHAMDULILLAH", "MENINGGAL", "SELAMAT TINGGAL", "BYE BYE", "JANGAN MASUK LAGI KAU"]
 const s = ["LEAVING", "OUT", "MATI", "KE RAHMATULLAH", "REMOVE"]
-const m = anu[Math.random(Math.floor() * anu.length)]
-const l = s[Math.random(Math.floor() * s.length)]
+const m = anu[Math.floor(Math.random() * anu.length)]
+const l = s[Math.floor(Math.random() * s.length)]
 module.exports = class Goodbye extends Greeting {
     constructor() {
         super();
