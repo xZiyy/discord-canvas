463d5c0e93be2c02adac66a16dc368efd01ea901
Hardianto~ <71861395+hardiantojek93@users.noreply.github.com>
2021-06-21 21:54:36 +0800

    Update Welcome.js

--- src/greetings/Welcome.js
+++ src/greetings/Welcome.js
@@ -1,8 +1,8 @@
 const Greeting = require("./Base");
 const m = ["SELAMAT DATANG", "WELCOME", "HAPPY JOIN", "NEW MEMBER", "NEW PARTICIPANT", "HELLO"]
 const l = ["Welcome to", "Thanks to join", "Selamat Datang Di"]
-const w = m[Math.random(Math.floor() * m.length)]
-const u = l[Math.random(Math.floor() * l.length)]
+const w = m[Math.floor(Math.random() * m.length)]
+const u = l[Math.floor(Math.random() * l.length)]
 module.exports = class Welcome extends Greeting {
     constructor() {
         super();
