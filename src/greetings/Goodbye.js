b33792407eb01d5e8fd7394eba2732d87238482d
Hardianto~ <71861395+hardiantojek93@users.noreply.github.com>
2021-06-21 11:10:59 +0800

    Update Goodbye.js

--- src/greetings/Goodbye.js
+++ src/greetings/Goodbye.js
@@ -1,10 +1,13 @@
 const Greeting = require("./Base");
-
+const anu = ["GOODBYE", "INNALILAHI", "ALHAMDULILLAH", "MENINGGAL"]
+const s = ["SI AJG", "SI BABI", "SI WIBU", "SI UDIN"]
+const m = Math.random(Math.floor() * anu.length) 
+const l = Math.random(Math.floor() * s.length)
 module.exports = class Goodbye extends Greeting {
     constructor() {
         super();
-        this.textTitle = "GOODBYE";
-        this.textMessage = "Leaving {server}";
+        this.textTitle = m;
+        this.textMessage = l+"{server}";
         this.colorTitle = "#df0909";
     }
 };
