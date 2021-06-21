0293ed08948ab682928839095b09f821b20d9f38
Hardianto~ <71861395+hardiantojek93@users.noreply.github.com>
2021-06-21 13:59:04 +0800

    Update Welcome.js

--- src/greetings/Welcome.js
+++ src/greetings/Welcome.js
@@ -1,10 +1,13 @@
 const Greeting = require("./Base");
-
+const m = ["SELAMAT DATANG", "WELCOME", "HAPPY JOIN", "NEW MEMBER", "NEW PARTICIPANT", "HELLO"]
+const l = ["Welcome to", "Thanks to join", "Selamat Datang Di"]
+const w = m.[Math.random(Math.floor() * m.length)]
+const u = l.[Math.random(Math.floor() * l.length)]
 module.exports = class Welcome extends Greeting {
     constructor() {
         super();
-        this.textTitle = "WELCOME";
-        this.textMessage = "Welcome to {server}";
+        this.textTitle = w;
+        this.textMessage = u + " {server}";
         this.colorTitle = "#03A9F4";
     }
 };
